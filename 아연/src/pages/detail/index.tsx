2fc271aada922f455aee886dd71107dbbeba90b4
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2024-05-03 18:50:33 +0900

    feat: add markup for detail page

--- 아연/src/pages/detail/index.tsx
+++ 아연/src/pages/detail/index.tsx
@@ -1,24 +1,60 @@
 import { getDetail, prefetchDetail } from '@/apis/detail'
+import Thumbnail from '@/components/Thumbnail'
 import { queryClient } from '@/pages/_app'
 
-import { Movie } from '@/typings'
+import { Genre, Movie } from '@/types/movie'
+import styled from '@emotion/styled'
 
 import { dehydrate, useQuery } from '@tanstack/react-query'
 import { GetServerSideProps } from 'next'
 
 interface DetailProps {
-  detail: any
+  detail: Movie
   id: Movie['id']
 }
 const Detail = ({ id }: DetailProps) => {
-  const { data } = useQuery({
+  const { data, isSuccess } = useQuery({
     queryKey: ['detail', id],
     queryFn: () => getDetail(id),
     staleTime: 1000000,
   })
+
   console.log(data)
 
-  return <div>{id}</div>
+  return (
+    <>
+      {isSuccess ? (
+        <>
+          <Thumbnail
+            imagePath={data.poster_path}
+            title={data.title}
+            width={500}
+          />
+          <Wrapper>
+            <div>
+              {data.genres.map((genre: Genre, idx: number) => (
+                <div key={idx}>{genre.name}</div>
+              ))}
+            </div>
+          </Wrapper>{' '}
+          <Wrapper>
+            <div>{data.title}</div>
+          </Wrapper>
+          <Wrapper>
+            <div>{data.original_language}</div>
+            <div>{data.release_date}</div>
+            <div>{data.runtime}</div>
+            <div>{data.vote_average}</div>
+          </Wrapper>
+          <Wrapper>
+            <div>{data.overview}</div>
+          </Wrapper>
+        </>
+      ) : (
+        <span>에러 남...</span>
+      )}{' '}
+    </>
+  )
 }
 
 export default Detail
@@ -41,3 +77,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
   }
 }
+
+const Wrapper = styled.div`
+  color: white;
+`
