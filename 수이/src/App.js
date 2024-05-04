78550668e2df04ca32ff700bbcf4e91f0627c15f
수니 <suni@suniui-noteubug.local>
2024-05-04 09:44:46 +0900

    2024.5.4

---
+++ 수이/src/App.js
@@ -0,0 +1,25 @@
+import Movie from './Components/Movie';
+import {dummy} from './movieDummy';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app-container'>
+        {
+          dummy.results.map((item)=>{
+            return(
+              <Movie 
+                title={item.title}
+                poster_path={item.poster_path}
+                vote_average={item.vote_average}
+              />
+            )
+          })
+        }
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
