03337826c9c30a3aeaedaa4800b277a07a6bc266
JangAyeon <ayeon3052@ewhain.net>
2024-05-16 11:58:09 +0900

    fix: add async for window scroll height

--- 아연/src/utils/scroll.ts
+++ 아연/src/utils/scroll.ts
@@ -7,6 +7,9 @@ export const getScrollY = () => {
   return Number(localStorage.getItem(SCROLL_Y_Key))
 }
 export const setScrollY = () => {
-  window.scrollTo({ top: getScrollY() })
-  localStorage.removeItem(SCROLL_Y_Key)
+  setTimeout(() => {
+    window.scrollTo({ top: getScrollY() })
+
+    localStorage.removeItem(SCROLL_Y_Key)
+  }, 1000)
 }
