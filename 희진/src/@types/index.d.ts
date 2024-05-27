fc7f3b51eac9a9e1d709ead938357d112364c359
heezindev <zqewe1@gmail.com>
2024-05-27 14:49:35 +0900

    즐겨찾기 기능구현

--- 희진/src/@types/index.d.ts
+++ 희진/src/@types/index.d.ts
@@ -8,14 +8,14 @@ export interface IMovieItem {
 export interface MovieResult {
   adult: boolean;
   backdrop_path: string;
-  genre_ids: number[];
+  genre_ids?: number[];
   id: number;
   original_language: string;
   original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: Date;
+  release_date: string;
   title: string;
   video: boolean;
   vote_average: number;
